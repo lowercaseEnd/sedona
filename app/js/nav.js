f6bc7a1aff8e8f0e584f431e5e5c95e5bda347e4
John Doe <randomEme@protonmail.com>
2019-12-26 16:55:39 +0400

    Изменил тег свг под навигацей с ogbject на img, увеличил размер фона с горами

--- app/js/nav.js
+++ app/js/nav.js
@@ -1,8 +1,10 @@
 "use strict"
 let mainNav = document.querySelector(".main-nav");
 let menuButton = document.querySelector(".menu-toggle");
+//if user has a js menu will hide
 mainNav.classList.remove("main-nav--nojs");
-
+mainNav.classList.add("main-nav--closed");
+mainNav.classList.remove("main-nav--opened");
 menuButton.addEventListener("click", () => {
   if (mainNav.classList.contains("main-nav--closed")) {
     mainNav.classList.remove("main-nav--closed");
